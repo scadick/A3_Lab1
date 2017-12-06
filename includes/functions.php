996ae44e1309c73b7ae3ea1474725e1a58a0a4b8
Cadick <s_cadick2@tss-hr420-dm09.fc.ca>
2017-12-06 14:57:24 -0500

    committted

--- includes/functions.php
+++ includes/functions.php
@@ -1,42 +1,45 @@
-<?php
-  // these are variables, just like JS variables
-  $user = "root"; //user name
-  $pass = "root"; // user password
-  $host = "localhost"; //host we're using (apache inside WAMP)
-  $db = "a3_cooperInfo"; //database we're connecting to
-
-// 1. create a connection to the database
+<?
+// do the connection first, then write a query
+$user = "root";
+$pass = "root";
+$host = "localhost";
+$db = "a3_cooperInfo";
 $conn = mysqli_connect($host, $user, $pass, $db);
-mysqli_set_charset($conn, 'utf8');
-// 2. test the connection
 if (!$conn) {
-  echo 'something broke, your connection will not work';
+  echo 'sumpin done gone wrong, son';
   exit;
 }
-
-// if everything connected, we'll see this:
-// echo 'connected, yo!';
+echo 'connected, yo!';
+// 1. do a select for all of the car data
 //
-$myQuery = "SELECT * FROM mainmodel"; // this is a simple SQL query
-$result = mysqli_query($conn, $myQuery); // result holds the result set
-$rows = array(); // we'll push each row into an array
-
-while($row = mysqli_fetch_assoc($result)) {
-  $rows[] = $row; // push each row from the result (the DB rows) into this aray
-}
-
-// let's see what we get!
+// $myQuery = "SELECT * FROM mainmodel";
+// $result = mysqli_query($conn, $myQuery);
+// $rows = array();
+//
+// while ($row = mysqli_fetch_assoc($result)) {
+//   $rows[] = $row;
+// };
+// echo back just the result set first
 //var_dump($rows);
+//echo $rows;
+//echo json_encode($rows); // create a JSON object from the result set
+// then echo back the JSON encoded result => after we set up the AJAX call in our .js file
+//echo json_encode($rows);
+//echo 'connected!';
 //
-if (isset($_GET['carModel'])) {
-  $car = $_GET['carModel'];
-
-  $myQuery = "SELECT * FROM mainmodel WHERE model = '$car'"; // this is a simple SQL query
-  $result = mysqli_query($conn, $myQuery); // result holds the result set
-
+//2. WRITE ANOTHER QUERY AND TEST IT IN THE BROWSER
+//talk about get supervariable
+if (isset($_GET["carModel"])) { // check to see if a query parameter exists
+  $car = $_GET["carModel"];
+  // pass in the car variable using the location bar in the browser (?carModel=F56)
+  $myQuery = "SELECT * FROM mainmodel WHERE model = '$car'";
+  // send the query
+  $result = mysqli_query($conn, $myQuery);
+  // get the result
   $row = mysqli_fetch_assoc($result);
-
+  // echo it back to whatever called it (the browser, to start)
   //var_dump($row);
+  // and then encode it for the javascript AJAX call
   echo json_encode($row);
-}
+};
 ?>
--- js/main.js
+++ js/main.js
@@ -1,72 +1,58 @@
 (function () {
-  // start with retrieving the elements from the page, and then adding event handling. then write the logic. refer to the seasons example / homework
-   var changeImg = document.querySelectorAll('.data-ref'),
-   changeModel = document.querySelector('.modelName'),
-   changePrice = document.querySelector('.priceInfo'),
-   changeDescription = document.querySelector('.modelDetails'),
-   appliedClass;
-   const httpRequest = new XMLHttpRequest();
+  var carButtons = document.querySelectorAll('.data-ref');
 
-   function getCarData() {
-     //1. make an AJAX call to the database; handle any errors first
-     if (!httpRequest) {
-       alert('giving up, your browser sucks!');
-       return false;
-     }
+  function getCarData() {
+    const url = './includes/functions.php?carModel=' + this.id;
 
-     httpRequest.onreadystatechange = processRequest; // fire this function
-     // when things change
-     httpRequest.open('GET', './includes/functions.php?carModel=' + this.id);
-     httpRequest.send();
-   }
+    // fetchAPI uses the Promise API (new for ES6)
+    //fetch(url) // this does the AJAX call
+    //    .then((resp) => resp.json())
+    //    .then((data) => { processResults(data); }
+    //    .catch(function(error) {
+    //      console.log(error);
+    //    });
+      fetch(url) // this does the AJAX call
+          .then((resp) => resp.json())
+          .then(({ modelName, pricing, modelDetails })) => {
+          let model = document.querySelector('.modelName').textContent = modelName;
+          let price = document.querySelector('.priceInfo').innerHTML = pricing;
+          let desc = document.querySelector('.modelDetails').textContent = modelDetails;
 
-   function processRequest() {
-     // handle the stages of our AJAX call
-     let reqIndicator = document.querySelector('.request-state');
-     reqIndicator.textContent = httpRequest.readyState;
+          carButtons.forEach(function(car, index) {
+            car.classList.add('nonActive');
+            });
+            document.querySelector(`#${data.model}`).classList.remove('nonActive');
+          }
+          .catch(function(error) {
+            console.log(error);
+          });
+  }
+  // processResult is run when the AJAX call is complete and we have the data back. It gets called on line 36, and the data variable gets passed in from that function (it's the JavaScript object we got from the database)
+  //function processResult(data) {
+    // destructure the data and extract only what we need
+    // the data is coming in as an object - it's 'structured' data. We can reach into it and pull out just the values we need by using a destructuring assignment
+    //const { modelName, pricing, modelDetails } = data;
 
-    //debugger;
+    // this is statement chaining - we can select an element and change its content all at once, instead of doing it in multiple steps
+    //let model = document.querySelector('.modelName').textContent = modelName;
+    //let price = document.querySelector('.priceInfo').innerHTML = pricing;
+    //let desc = document.querySelector('.modelDetails').textContent = modelDetails;
 
-     if (httpRequest.readyState === XMLHttpRequest.DONE) {
-       if (httpRequest.status === 200) { // request worked, all is good!
-         //debugger;
-         let data = JSON.parse(httpRequest.responseText);
-         processResult(data);
-       } else {
-         alert('There was a problem with the request');
-       }
-     }
-   }
+    // loop through all the car thumbnails again and add a nonActive class to them to fade them out. a forEach function uses the collection we set up in our variable declaration at the top on line 5, and passes each element in one at a time (that's what the car in the round brackets is). It also tracks what iteration of the loop it's on with the index variable (0, 1, 2 etc), which we're not using in the function.
+    //carButtons.forEach(function(car, index) {
+    //  car.classList.add('nonActive');
+    //});
 
- function processResult (data) {
-   debugger;
-   const{modelName, pricing, modelDetails} = data;
+    //this.classList.remove('nonActive');
+    // the "this" keyword won't work any more because we're out of scope (we'll get into scope and what it means in the winter term). we need to select the current element another way - the model field is part of the dataset we got back from the database, and it's also the ID of the current model (they match) so we can use that to select the element on the page using a JavaScript template string
+    //
+    // template strings are new in ES6; among other things, they let you dig in to data and pull out values, create HTML blobs in JavaScript files, and generally make life a lot easier for JS developers who have had to write hacky ways to do things like this
+    //document.querySelector(`#${data.model}`).classList.remove('nonActive');
+  }
 
-	 // swap Model of car
- //let modelSwap = document.querySelector('.modelInfo');
- //let objectIndex = carData[this.id];
- let model = document.querySelector('.modelName').textContent = modelName;
- let price = document.querySelector('.priceInfo').innerHTML = pricing;
- let desc = document.querySelector('.modelDetails').textContent = modelDetails;
-
- //changeModel.firstChild.nodeValue = objectIndex.modelName;
- //changePrice.firstChild.nodeValue = objectIndex.priceInfo;
- //changeDescription.firstChild.nodeValue = objectIndex.modelDetails;
-
- //appliedClass = this.id;
-
- console.log(this.id);
- 	//opacity changes for images
-	changeImg.forEach(function(element, index){
-	element.classList.remove("focusMini");
-	});
-	//this.classList.add("focusMini");
-  document.querySelector(`#${data.model}`).classList.add("focusMini");
- }
- changeImg.forEach(function(element, index){
-
-	element.addEventListener('click', getCarData, false);
-
- });
+  // loop through and add event handling to each car thumbnail on the page. on a click, they'll fire the AJAX call at the top of the script file.
+  carButtons.forEach(function(car, index) {
+    car.addEventListener('click', getCarData, false);
+  });
 
 })();
